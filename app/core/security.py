b3ed3f50530f061bc3de6c789fb9dcfa6d1e3f47
Ashish Nallana <ashishnallana@gmail.com>
2026-04-18 15:09:40 +0530

    fixed the bcrypt error.

--- app/core/security.py
+++ app/core/security.py
@@ -1,16 +1,21 @@
 from datetime import datetime, timedelta
 from typing import Any, Union
 from jose import jwt
-from passlib.context import CryptContext
 from app.core.config import settings
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+import bcrypt
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+    password_byte_enc = plain_password.encode('utf-8')
+    hashed_password_byte_enc = hashed_password.encode('utf-8')
+    # Use [:72] to manually truncate over 72 bytes to avoid ValueError with long passwords
+    return bcrypt.checkpw(password_byte_enc[:72], hashed_password_byte_enc)
 
 def get_password_hash(password: str) -> str:
-    return pwd_context.hash(password)
+    pwd_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    # Use [:72] to manually truncate over 72 bytes to avoid ValueError
+    hashed_password = bcrypt.hashpw(pwd_bytes[:72], salt)
+    return hashed_password.decode('utf-8')
 
 def create_access_token(subject: Union[str, Any], role: str) -> str:
     expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
